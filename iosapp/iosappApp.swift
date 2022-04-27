8ad63b529b56dd21bbff12b78ba3b2dfa7e5b861
Rohit Singh <73903627+rohitttsingh@users.noreply.github.com>
2022-04-28 00:10:24 +0530

    Initial Commit

---
+++ iosapp/iosappApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosappApp.swift
+//  iosapp
+//
+//  Created by Rohit Singh on 28/04/22.
+//
+
+import SwiftUI
+
+@main
+struct iosappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
