3c3db1e89fe40119990e5f44f1b12e499aa9ccf0
Rohit Singh <73903627+rohitttsingh@users.noreply.github.com>
2022-04-28 01:13:49 +0530

    v 1.1.0

--- iosapp/ContentView.swift
+++ iosapp/ContentView.swift
@@ -8,12 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var username: String = ""
+    @State var isPrivate: Bool = true
+    @State var notificationsEnabled: Bool = false
+    @State private var previewIndex = 0
+    var previewOptions = ["Always", "When Unlocked", "Never"]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("PROFILE")) {
+                    TextField("Username", text: $username)
+                    TextField("Password", text: $username)
+
+                    Toggle(isOn: $isPrivate) {
+                        Text("Private Account")
+                    }
+                }
+                
+                Section(header: Text("NOTIFICATIONS")) {
+                    Toggle(isOn: $notificationsEnabled) {
+                        Text("Enabled")
+                    }
+                    Picker(selection: $previewIndex, label: Text("Show Previews")) {
+                        ForEach(0 ..< previewOptions.count) {
+                            Text(self.previewOptions[$0])
+                        }
+                    }
+                }
+                
+                Section(header: Text("ABOUT")) {
+                    HStack {
+                        Text("Version")
+                        Spacer()
+                        Text("2.2.1")
+                    }
+                }
+                
+                Section {
+                    Button(action: {
+                        print("Perform an action here...")
+                    }) {
+                        Text("Submit All information")
+                    }
+                }
+            }
+            .navigationBarTitle("Create Account")
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
